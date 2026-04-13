a901a329cfacc3ce9b7d4cb5c4496a31d14eeed9
Tako <o0937492087@gmail.com>
2026-04-13 17:46:03 +0800

    更新套件檔

--- ios/TPDirect.xcframework/ios-arm64_armv7/TPDirect.framework/Headers/TPDIPassMoneyResult.h
+++ ios/TPDirect.xcframework/ios-arm64_armv7/TPDirect.framework/Headers/TPDIPassMoneyResult.h
@@ -1,20 +1,22 @@
 //
-//  TPDAtomeResult.h
+//  TPDIPassMoneyResult.h
 //  TPDirect
 //
-//  TPDirect iOS SDK - v2.14.0
-//  Created by Cherri Kevin on 12/27/21.
-//  Copyright © 2021 tech.cherri. All rights reserved.
+//  Created by Kevin Kao on 2025/5/5.
+//  Copyright © 2025 tech.cherri. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface TPDAtomeResult : NSObject
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TPDIPassMoneyResult : NSObject
 
 @property (strong, nonatomic) NSString * recTradeId;
 @property (strong, nonatomic) NSString * orderNumber;
 @property (assign, nonatomic) NSInteger  status;
 @property (strong, nonatomic) NSString * bankTransactionId;
 
-
 @end
+
+NS_ASSUME_NONNULL_END
